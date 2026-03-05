e8aa75caabfbe8248629e540e453f44d1452fee7
Luc Boily <luc.boily@gmail.com>
2026-03-05 15:28:00 -0500

    Production update

--- src/app/api/send-sms/route.ts
+++ src/app/api/send-sms/route.ts
@@ -4,8 +4,8 @@ import { createClient } from "@supabase/supabase-js";
 
 // Supabase setup
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
 
 const client = twilio(
